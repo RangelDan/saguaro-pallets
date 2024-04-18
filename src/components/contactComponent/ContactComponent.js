2a64bf7e6f5d689b0be1c41b70caf5b33820893a
RangelDan <danielrrangel1994@gmail.com>
2024-04-17 20:22:51 -0700

    contact

--- src/components/contactComponent/ContactComponent.js
+++ src/components/contactComponent/ContactComponent.js
@@ -7,9 +7,9 @@ const ContactComponent = () => {
         <div className={styles.container}>
             <h1>Servicing all of metro Phoenix and more!</h1>
             <div className={styles.info}>
-                <p>Email: info@yourcompany.com</p>
-                <p>Phone: (123) 456-7890</p>
-                <p>Address: 1234 Street Rd, Your City, State, 12345</p>
+                <p>Email: Saguaropallets@gmail.com</p>
+                <p>Phone: (480) 469-5678</p>
+                <p>Address: El Mirage, AZ, 85335</p>
             </div>
         </div>
     );
